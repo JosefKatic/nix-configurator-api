10f455334dbfc3953306569f8726209e530c74ec
Josef Katič <josef@joka00.dev>
2024-11-28 21:57:00 +0100

    Use flake-parts

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
   inputs = {
     dream2nix.url = "github:nix-community/dream2nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
@@ -12,42 +13,64 @@
     self,
     dream2nix,
     nixpkgs,
+    flake-parts,
     ...
-  } @ inputs: let
-    # A helper that helps us define the attributes below for
-    # all systems we care about.
-    eachSystem = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-  in {
-    devShells = eachSystem (system:
-      import ./shell.nix {
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
-      });
-    formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
-    nixosModules.default = eachSystem (system:
-      import ./nix/module.nix inputs {
-        inherit (nixpkgs) lib;
-        pkgs = nixpkgs.legacyPackages.${system};
-      });
-    packages = eachSystem (system: {
-      web-config-api = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./nix/package.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        pkgs,
+        inputs',
+        config,
+        lib,
+        system,
+        ...
+      }: let
+        web-config-api = dream2nix.lib.evalModules {
+          packageSets.nixpkgs = pkgs;
+          modules = [
+            # Import our actual package definiton as a dream2nix module from ./default.nix
+            ./nix/package.nix
+            {
+              # Aid dream2nix to find the project root. This setup should also works for mono
+              # repos. If you only have a single project, the defaults should be good enough.
+              paths.projectRoot = ./.;
+              # can be changed to ".git" or "flake.nix" to get rid of .project-root
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        };
+      in {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        packages = {
+          inherit web-config-api;
+          default = web-config-api;
+        };
       };
-    });
-  };
+      flake.nixosModules = {
+        default = import ./nix/module.nix inputs;
+      };
+    };
 }
+#   let
+#     # A helper that helps us define the attributes below for
+#     # all systems we care about.
+#     eachSystem = nixpkgs.lib.genAttrs [
+#       "x86_64-linux"
+#       "aarch64-linux"
+#     ];
+#   in {
+#     devShells = eachSystem (system:
+#       import ./shell.nix {
+#         pkgs = nixpkgs.legacyPackages.${system};
+#         lib = nixpkgs.lib;
+#       });
+#     formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+#     });
+#   };
+# }
+
--- shell.nix
+++ shell.nix
@@ -1,13 +1,18 @@
-{pkgs ? import <nixpkgs> {}, ...}: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
-    packages = with pkgs; [
-      nix
-      git
-      python3
-      nodejs_22
-      nodePackages.webpack
-      nodePackages.webpack-cli
-    ];
+{
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: {
+    devShells.default = pkgs.mkShell {
+      NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
+      packages = with pkgs; [
+        nix
+        git
+        python3
+        nodejs_22
+      ];
+    };
+    formatter = pkgs.alejandra;
   };
 }
